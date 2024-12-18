cf15700e83b1390ec657d00a0d3f2b86a7601a61
Alex Vakhov <alex.vakhov@gmail.com>
2024-12-18 19:39:18 +0500

    update index.html

--- static/script.js
+++ static/script.js
@@ -6,36 +6,64 @@ app.controller('proxyTableCtrl', function ($scope, $http) {
     $scope.search = {};
     $scope.title = "Loading...";
 
-    $http.get('proxylist.json').then((response) => {
-        json = response.data;
-        json.forEach(proxy => {
-            switch (proxy.anon) {
-                case "1":
-                    proxy.anon_verbose = "No anonymity";
-                    break;
-                case "2":
-                    proxy.anon_verbose = "Low anonymity";
-                    break;
-                case "3":
-                    proxy.anon_verbose = "Average anonymity";
-                    break;
-                case "4":
-                    proxy.anon_verbose = "High anonymity";
-                    break;
-            }
-            proxy.type = "";
+    const cacheKey = "proxyData";
+    const cacheTimestampKey = "proxyDataTimestamp";
+    const updateInterval = 10 * 60 * 1000; // 10 минут
 
-            if (proxy.http === "1")
-                proxy.type = "http";
-            if (proxy.ssl === "1")
-                proxy.type = proxy.type.concat(" https");
-            if (proxy.socks4 === "1")
-                proxy.type = proxy.type.concat(" socks4");
-            if (proxy.socks5 === "1")
-                proxy.type = proxy.type.concat(" socks5");
+    const cachedData = localStorage.getItem(cacheKey);
+    const cachedTimestamp = localStorage.getItem(cacheTimestampKey);
 
-            $scope.data.push(proxy);
-        });
+    function mapAnonymityLevel(level) {
+        switch (level) {
+            case "1":
+                return "No anonymity";
+            case "2":
+                return "Low anonymity";
+            case "3":
+                return "Average anonymity";
+            case "4":
+                return "High anonymity";
+            default:
+                return "Unknown";
+        }
+    }
+
+    if (cachedData && cachedTimestamp && Date.now() - cachedTimestamp < updateInterval) {
+        $scope.data = JSON.parse(cachedData);
         $scope.title = "Proxies";
-    })
+    } else {
+        $http.get('proxylist.json').then((response) => {
+            const json = response.data.map(proxy => ({
+                ...proxy,
+                anon_verbose: mapAnonymityLevel(proxy.anon),
+                type: [
+                    proxy.http === "1" && "http",
+                    proxy.ssl === "1" && "https",
+                    proxy.socks4 === "1" && "socks4",
+                    proxy.socks5 === "1" && "socks5"
+                ].filter(Boolean).join(" ")
+            }));
+
+            $scope.data = json;
+            $scope.title = "Proxies";
+
+            localStorage.setItem(cacheKey, JSON.stringify(json));
+            localStorage.setItem(cacheTimestampKey, Date.now());
+        });
+    }
+
+    $scope.clearCache = function () {
+        localStorage.removeItem(cacheKey);
+        localStorage.removeItem(cacheTimestampKey);
+        location.reload();
+    };
+
+    setInterval(() => {
+        const cachedTimestamp = localStorage.getItem(cacheTimestampKey);
+        if (!cachedTimestamp || Date.now() - cachedTimestamp >= updateInterval) {
+            localStorage.removeItem(cacheKey);
+            localStorage.removeItem(cacheTimestampKey);
+            location.reload();
+        }
+    }, updateInterval);
 });
